d12dc67427d3d3e2b80c1ec1d155e7e57fe3afef
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-08-31 21:39:14 +0200

    Add files via upload

---
+++ WeaponAnimationMenu/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Don't change anything. I won't help with this  if it breaks.
+fx_version "cerulean"
+games {"gta5"}
+
+title "Weapon Animatoin Menu"
+description "Aim and holster animation menu"
+author "Andyyy#7666"
+version "v1.0"
+
+client_scripts {
+    "NativeUI.lua",
+    "config.lua",
+    "functions.lua",
+    "menu.lua",
+}
